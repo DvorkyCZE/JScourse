204d67da507e4e1b7728bde03fba41f5ac68dcbf
Dvorky <marekdvorak.job@gmail.com>
2021-08-03 22:02:34 +0200

    My first testing commit

---
+++ 01-Fundamentals-Part-1/starter/script_testing.js
@@ -0,0 +1,45 @@
+
+const myCountry = 'Czech Republic';
+const myContinent = 'Europe';
+const myLanguage = 'czech';
+let populationOfCzechRepublic = 10;
+populationOfCzechRepublic++;
+let populationOfFinland = 6;
+let averagePopulation = 33;
+
+console.log(populationOfCzechRepublic / 2);
+console.log(populationOfCzechRepublic > populationOfFinland);
+console.log(populationOfCzechRepublic < averagePopulation);
+
+var description = myCountry + ' is in ' + myContinent + ', and its ' + populationOfCzechRepublic + ' million people speak ' + myLanguage;
+
+console.log(description);
+console.log(`${myCountry} is in
+${myContinent}, and its 
+${populationOfCzechRepublic} million people speak ${myLanguage}`);
+/*
+let massMark1 = 78;
+let heightMark1 = 1.69;
+let massMark2 = 95;
+let heightMark2 = 1.88;
+
+let massJohn1 = 92;
+let heightJohn1 = 1.95;
+let massJohn2 = 85;
+let heightJohn2 = 1.76;
+
+const BMIMark1 = massMark1 / (heightMark1 ** 2);
+const BMIMark2 = massMark2 / (heightMark2 ** 2);
+
+const BMIJohn1 = massJohn1 / (heightJohn1 ** 2);
+const BMIJohn2 = massJohn2 / (heightJohn2 ** 2);
+console.log(BMIMark1, BMIJohn1);
+console.log(BMIMark2, BMIJohn2);
+
+const markHigherBMI1 = BMIMark1 > BMIJohn1;
+const markHigherBMI2 = BMIMark2 > BMIJohn2;
+
+console.log(markHigherBMI1);
+console.log(markHigherBMI2);
+
+*/
